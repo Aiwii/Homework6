0b7d29cf781d1e19bfdce24f6fabb6445c897b64
Aiwii <abakar10.08@mail.ru>
2024-08-03 11:42:23 +0500

    rework

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,7 @@ public class Main {
 
         int savings = 29000;
         int totalSavings = 0;
-        for (int i = 1; i < 12; i ++) {
+        for (int i = 1; i <= 12; i ++) {
             totalSavings = totalSavings + savings;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavings + " рублей.");
         }
